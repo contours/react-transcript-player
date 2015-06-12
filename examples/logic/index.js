357463d3d3888b2b681f191c90482ce3fc0c0b37
Ryan Shaw <ryanshaw@unc.edu>
2015-06-12 13:40:33 -0400

    Removed comment.

--- examples/logic/index.js
+++ examples/logic/index.js
@@ -4,7 +4,6 @@ var React = require('react')
   , TranscriptPlayer = require('react-transcript-player')
   , transcript = require('./media/transcript.json')
 
-//React.render(<TranscriptPlayer transcript={transcript}/>, document.body)
 React.render(
   React.createElement(TranscriptPlayer, {transcript: transcript}),
   document.getElementById('mount'))
