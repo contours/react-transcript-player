0542193eadfaa83487d88f776bbc7b2ab6d3bf23
Ryan Shaw <ryanshaw@unc.edu>
2015-06-16 16:00:49 -0400

    Better handling of long transcripts with various alignment problems.

--- src/player.jsx
+++ src/player.jsx
@@ -6,7 +6,7 @@ var React = require('react')
 
 module.exports = React.createClass(
   { getInitialState: function() {
-      return {time: 0, seekTime: null}
+      return {time: 0, seekTime: null, ended: false}
     }
   , handleTimeUpdate: function(time) {
       if (this.state.seekTime !== null &&
@@ -16,32 +16,37 @@ module.exports = React.createClass(
         this.setState({time: time})
       }
     }
+  , handleEnded: function(ended) {
+      this.setState({ended: ended})
+    }
   , handleSeekRequest: function(seekTime) {
-      this.setState({seekTime: seekTime})
+      this.setState({seekTime: seekTime, ended: false})
     }
   , render: function() {
       var style =
         { display: "flex"
         , flexFlow: "column nowrap"
-        , width: 400
+        , width: 600
         }
       return (
       <div style={style}>
         <style scoped>{`
-.turn { padding: 0.25em }
+.turn { padding: 0.25em; color: rgba(28,28,28,1); transition: color .7s ease }
 .turn:nth-child(odd) { background-color: #f1f1f1 }
 .speaker { margin-right: 0.5em; font-weight: bold }
-.word { cursor: pointer }
-.word:hover { background-color: #d0effe;
-              outline: 0.2em solid #d0effe }
+.speech { cursor: pointer; color: rgba(28,28,28,1); transition: color .7s ease }
+.speech:hover { background-color: #d0effe;
+                outline: 0.2em solid #d0effe }
+.played { color: rgba(28,28,28,0.3) }
 `}</style>
         <AudioPlayer
           media={this.props.transcript.media}
           seekTime={this.state.seekTime}
-          onTimeUpdate={this.handleTimeUpdate} />
+          onTimeUpdate={this.handleTimeUpdate}
+          onEnded={this.handleEnded} />
         <TranscriptView
           transcript={this.props.transcript}
-          time={this.state.time}
+          time={this.state.time} ended={this.state.ended}
           onSeekRequest={this.handleSeekRequest} />
       </div>
       )
